990f663d1715c76b8738269749f57cb6fffae790
Abdulrahman Sameer <81337359+goro5685@users.noreply.github.com>
2024-03-05 18:07:13 +0300

    Update urls.py

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from .views import index, about, menu, menu_item, bookings, Book
+#NOTE That the URL of the api-token-auth is on the urls.py on the project level
 
 urlpatterns = [
     path('', index , name='home'),
